77253c842bcc5f4897a8813aff3d885bd5498d26
Prashanth2025 <prashanthramesh.02@gmail.com>
2025-11-10 14:56:23 +0530

    all files

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -7,12 +7,23 @@ function Navbar({ darkMode, setDarkMode }) {
         <span className="navbar-brand fw-bold fs-3 user-select">
           <i className="bi bi-backpack-fill me-2"></i>Item Packer
         </span>
-        <button
-          className="btn btn-sm btn-light"
-          onClick={() => setDarkMode(!darkMode)}
-        >
-          {darkMode ? "☀️ Light Mode" : "🌙 Dark Mode"}
-        </button>
+        <div className="d-flex justify-content-center">
+          <button
+            className="btn btn-sm btn-light me-2"
+            onClick={() => setDarkMode(!darkMode)}
+          >
+            {darkMode ? "☀️ Light Mode" : "🌙 Dark Mode"}
+          </button>
+          <a
+            href="https://github.com/Prashanth2025/Item_Packer"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-sm btn-light d-inline-flex align-items-center"
+          >
+            <i className="bi bi-code-square me-2"></i>
+            Click for Source
+          </a>
+        </div>
       </div>
     </nav>
   );
